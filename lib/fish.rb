5d37dc750cd66c683c2d4aead31b4701530250ea
haleykeizur <haleykz@yahoo.com>
2018-07-27 17:09:44 +0000

    Done.

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,18 @@
 class Fish
-  # code goes here
+  def initialize(name)
+    @name = name 
+    @mood = "nervous"
+  end 
+  
+  attr_reader :name
+  attr_writer :mood
+  
+  def name
+    @name 
+  end 
+  
+  def mood
+    @mood
+  end 
+  
 end
